a880b9f80299ac91371c391d70eb4c3c846b0209
StarShiningSnow <135973442+StarShiningSnow@users.noreply.github.com>
2026-06-29 04:41:14 +0800

    Add files via upload

--- mod/ask.py
+++ mod/ask.py
@@ -1,6 +1,14 @@
-
+from ai import ai_main,mode
 
 def setup(tree):
     @tree.command(name="ask",description="詢問AI")
     async def ask(interaction,question:str):
-        await interaction.response.send_message(question)
+
+        reply = await ai_main.chat(
+            user_id=interaction.user.id,
+            username=interaction.user.display_name,
+            message=question,
+            mode=mode.AIMode.ASK,
+        )
+
+        await interaction.response.send_message(reply)
