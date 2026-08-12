a7624081fef291062c90351aebe6653af0c14d39
StarShiningSnow <135973442+StarShiningSnow@users.noreply.github.com>
2026-08-13 05:38:34 +0800

    Add files via upload

---
+++ status.py
@@ -0,0 +1,17 @@
+from mcstatus import JavaServer
+import discord,secret
+
+def setup(tree):
+    @tree.command(description="皇帝陛下、総理閣下、並びに高官の皆様へ報告です。帝國情報ネットワークは極めて円滑に稼働しております！ (⁠✧⁠ω⁠✧⁠)")
+    async def status(interaction):
+        await interaction.response.defer()
+        server = JavaServer.lookup(secret.lan_ip)
+        status = server.status()
+        embed = discord.Embed(title="🎮 帝国情報網 稼働状況報告",color=discord.Color.green(),description=f"```\n{status.description}\n```")
+        embed.add_field(name="🌐 帝国接続拠点",value=f"`{secret.wan_ip}`",inline=False)
+        embed.add_field(name="📌 運用規格",value=f"`{status.version.name}`",inline=True)
+        embed.add_field(name="👥 領域内滞在者",value=f"`{status.players.online} / {status.players.max}`",inline=True)
+        embed.add_field(name="🟢 伝達遅延",value=f"`{round(status.latency,1)} ms`",inline=True)
+        embed.add_field(name="🧑‍💻 監視対象名簿",value=f"```\n{'\n'.join(p.name for p in status.players.sample)if status.players.sample else '該当者なし'}\n```",inline=False)
+        embed.set_footer(text="⚙️ 連邦省情報局管轄システム • 天皇陛下万歳！")
+        await interaction.followup.send(embed=embed)
